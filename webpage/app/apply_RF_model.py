58cbf77e2359e3b253bf57bcfa4bc2f0e701784e
Mike Kuhlen <kuhlen@gmail.com>
2013-09-13 13:47:33 -0700

    A few more updates and fixes. Pretty much ready to go.

--- webpage/app/apply_RF_model.py
+++ webpage/app/apply_RF_model.py
@@ -1,88 +1,131 @@
 import cPickle
-import time
+import time, os
 import datetime
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
-def apply_RF_model(flights, origin):
+hdf = pd.read_fwf('data/US_holidays_1997-2020.txt',colspecs=[[0,2],[3,5],[6,10],[12,50]],header=None,names=['Month','Day','Year','Holiday'])
+hdf['Holiday'] = hdf['Holiday'].apply(lambda x: x[:-1])
+holiday_dates = hdf.apply(lambda x: datetime.date(x['Year'],x['Month'],x['Day']),axis=1).values
+del hdf
+    
+def apply_RF_model(flights, origin, destination):
     """
     flights is a DataFrame.
     """
 
-    hdf = pd.read_fwf('data/US_holidays_1997-2020.txt',colspecs=[[0,2],[3,5],[6,10],[12,50]],header=None,names=['Month','Day','Year','Holiday'])
-    hdf['Holiday'] = hdf['Holiday'].apply(lambda x: x[:-1])
-    holiday_dates = hdf.apply(lambda x: datetime.date(x['Year'],x['Month'],x['Day']),axis=1).values
-    del hdf
+    ### *** Origin model ***
+    Pdelay_orig = {}
 
-    ## origins = flights['Origin'].unique()
-    ## for orig in origins:
-
-    #### GET RID OF THIS LOOP!
-    
-    ## for orig in ['SFO']:
-    for orig in [origin]:
-
-        print 'Unpickling RandomForest model...'
+    filename = 'RandomForest_models/by_origin/rfm_%s.pkl' % origin
+    if os.path.isfile(filename):
+        
+        print 'Unpickling RandomForest model for Origin...'
         time0 = time.time()       
-        f = open('RandomForest_models/by_origin/rfm_%s.pkl' % orig, 'rb')
-        (rfor,model_summary) = cPickle.load(f)
+        f = open(filename, 'rb')
+        (rfor,model_summary_orig) = cPickle.load(f)
         f.close()
         print '   That took %.1f seconds.' % (time.time() - time0)
 
-        ## set to single core for model evaluation
+        ## add feature importance to models
+        model_summary_orig['feature_importances'] = rfor.feature_importances_
+        
+        ### set to single core for model evaluation
         rfor.n_jobs = 1
+    
+        train_cols = model_summary_orig['training_columns']
+    
+        ### Create Origin flights DataFrame        
+        df = flights[ flights['Origin'] == origin ]
+        df, FlightID = fill_dataframe(df,train_cols)
         
-        train_cols = model_summary['training_columns']
-        
-        df = flights[ flights['Origin'] == orig ]
-        df['Year'] = df['DepartureDateTime'].apply(lambda x: x.year)
-        df['Month'] = df['DepartureDateTime'].apply(lambda x: x.month)
-        df['DayOfMonth'] = df['DepartureDateTime'].apply(lambda x: x.day)
-        df['DayOfWeek'] = df['DepartureDateTime'].apply(lambda x: x.weekday()+1)
-        df['DayOfYear'] = df['DepartureDateTime'].apply(lambda x: x.timetuple().tm_yday)
-        df['Week'] = df['DayOfYear'] / 7 + 1
-        df['ScheduledDepartureHour'] = df['DepartureDateTime'].apply(lambda x: x.hour)
-        df['ScheduledArrivalHour'] = df['ArrivalDateTime'].apply(lambda x: x.hour)
-        df['DaysFromNearestHoliday'] = df['DepartureDateTime'].apply(lambda x: abs(holiday_dates - x.date()).min().days)
-
-        FlightID = df['FlightID'].values
-        
-        df = df.drop(['ArrivalDate','ArrivalDateAdjustment','ArrivalDateTime','ArrivalTime','Carrier','DepartureDate','DepartureDateAdjustment','DepartureDateTime','DepartureTime','FlightID','FlightNumber','Origin'],axis=1)
-                
-        df.rename(columns={'FlightDuration':'ScheduledElapsedTime',
-                           'CarrierCode':'Carrier'}, inplace=True)
-
-
-        dummies = pd.get_dummies(df['Carrier'],prefix='Carrier')
-        df = df.join(dummies.ix[:,1:])
-
-        dummies = pd.get_dummies(df['Destination'],prefix='Destination')
-        df = df.join(dummies.ix[:,1:])
-        
-
-        ### Add zero columns for any missing columns
-        for tc in train_cols:
-            if tc not in df.columns:
-                df[tc] = np.zeros_like(df['Year'],dtype=float)
-
-        ### Drop any columns that are not in train_cols
-        cols_to_drop = []
-        for c in df.columns:
-            if c not in train_cols:
-                cols_to_drop.append(c)
-        df = df.drop(cols_to_drop,axis=1)
-
-
-        ### re-order the columns
-        df = df.ix[:, train_cols]                
-
-
         ### at last, apply the model
-        Pdelay = dict( zip(FlightID, rfor.predict_proba(df.values)[:,1]) )
+        Pdelay_orig = dict( zip(FlightID, rfor.predict_proba(df.values)[:,1]) )
+    
+        del df, FlightID, rfor
+    else:
+        print 'Could not find Origin model (%s), skipping.' % filename
+
+    ### *** Destination model ***
+    Pdelay_dest = {}
+    model_summary_dest = {}
+    
+    filename = 'RandomForest_models/by_destination/rfm_%s.pkl' % destination
+    if os.path.isfile(filename):
         
+        print 'Unpickling RandomForest model for Destination...'
+        time0 = time.time()       
+        f = open(filename, 'rb')
+        (rfor,model_summary_dest) = cPickle.load(f)
+        f.close()
+        print '   That took %.1f seconds.' % (time.time() - time0)
+
+        ## add feature importance to models
+        model_summary_dest['feature_importances'] = rfor.feature_importances_
+
+        ### set to single core for model evaluation
+        rfor.n_jobs = 1
+
+        train_cols = model_summary_dest['training_columns']
+    
+        ### Create Destination flights DataFrame        
+        df = flights[ flights['Destination'] == destination ]
+        df, FlightID = fill_dataframe(df,train_cols)
+    
+        ### at last, apply the model
+        Pdelay_dest = dict( zip(FlightID, rfor.predict_proba(df.values)[:,1]) )
+
+        del df, FlightID, rfor
+    else:
+        print 'Could not find Destination model (%s), skipping.' % filename
+
+    
+    return Pdelay_orig, Pdelay_dest, model_summary_orig, model_summary_dest
+
+
+def fill_dataframe(df, train_cols):
+    df['Year'] = df['DepartureDateTime'].apply(lambda x: x.year)
+    df['Month'] = df['DepartureDateTime'].apply(lambda x: x.month)
+    df['DayOfMonth'] = df['DepartureDateTime'].apply(lambda x: x.day)
+    df['DayOfWeek'] = df['DepartureDateTime'].apply(lambda x: x.weekday()+1)
+    df['DayOfYear'] = df['DepartureDateTime'].apply(lambda x: x.timetuple().tm_yday)
+    df['Week'] = df['DayOfYear'] / 7 + 1
+    df['ScheduledDepartureHour'] = df['DepartureDateTime'].apply(lambda x: x.hour)
+    df['ScheduledArrivalHour'] = df['ArrivalDateTime'].apply(lambda x: x.hour)
+    df['DaysFromNearestHoliday'] = df['DepartureDateTime'].apply(lambda x: abs(holiday_dates - x.date()).min().days)
+
+    FlightID = df['FlightID'].values
+        
+    df = df.drop(['ArrivalDate','ArrivalDateAdjustment','ArrivalDateTime','ArrivalTime','Carrier','DepartureDate','DepartureDateAdjustment','DepartureDateTime','DepartureTime','FlightID','FlightNumber','Origin'],axis=1)
                 
-    return Pdelay
+    df.rename(columns={'FlightDuration':'ScheduledElapsedTime',
+                       'CarrierCode':'Carrier'}, inplace=True)
+
+    dummies = pd.get_dummies(df['Carrier'],prefix='Carrier')
+    df = df.join(dummies.ix[:,1:])
+
+    dummies = pd.get_dummies(df['Destination'],prefix='Destination')
+    df = df.join(dummies.ix[:,1:])
+
+
+    
+    ### Add zero columns for any missing columns
+    for tc in train_cols:
+        if tc not in df.columns:
+            df[tc] = np.zeros_like(df['Year'],dtype=float)
+
+    ### Drop any columns that are not in train_cols
+    cols_to_drop = []
+    for c in df.columns:
+        if c not in train_cols:
+            cols_to_drop.append(c)
+    df = df.drop(cols_to_drop,axis=1)
+
+    ### re-order the columns
+    df = df.ix[:, train_cols]                
+    
+    return df, FlightID
 
 
 def dummy_train_cols():
--- webpage/app/views.py
+++ webpage/app/views.py
@@ -84,25 +84,48 @@ def results():
     flightstats = gfs.parse_flightstats_json(fs_json)
     flights = DataFrame( gfs.flatten_flightstats(flightstats) )
 
-    Pdelay_dict = am.apply_RF_model(flights,request_info['origin'])
+    Pdelay_dict_orig, Pdelay_dict_dest, model_summary_orig, model_summary_dest = am.apply_RF_model(flights,request_info['origin'],request_info['destination'])
 
-    assign_Pdelay = lambda x: Pdelay_dict[x] if x in Pdelay_dict.keys() else -1.0
 
+    def assign_Pdelay(fid, Pdelay_dict_orig, Pdelay_dict_dest):
+        if fid in Pdelay_dict_dest.keys():
+            return Pdelay_dict_dest[fid]
+        elif fid in Pdelay_dict_orig.keys():
+            return Pdelay_dict_orig[fid]
+        else:
+            return -1.0
+    
     color_norm = Normalize(0.0,0.5)
     
     for fs in flightstats:
-        ## Pdelay = [ Pdelay_dict[x] for x in fs['FlightID'] ]
-        Pdelay = [ assign_Pdelay(x) for x in fs['FlightID'] ]
-        ## Pdelay = np.random.uniform(size=len(fs['FlightID']))
+
+        ### Pdelay = np.random.uniform(size=len(fs['FlightID']))
+
+        Pdelay = [ assign_Pdelay(x, Pdelay_dict_orig, Pdelay_dict_dest) for x in fs['FlightID'] ]
         
         fs['IconColor'] = [rgb2hex( cmap(color_norm(x)) ) for x in Pdelay]
         fs['DelayProbability'] = ['%.1f%%' % (100.0*x) for x in Pdelay]
 
-            
+
+    ### model summaries
+    model_summaries = {}
+
+    if model_summary_orig:       
+        importance, feature = zip(* sorted( zip(model_summary_orig['feature_importances'],model_summary_orig['training_columns']), reverse=True ) )
+        model_summary_orig['top5_features'] = [(feature[i],importance[i]) for i in xrange(5)]
+        model_summaries['origin'] = model_summary_orig
+
+    if model_summary_dest:
+        importance, feature = zip(* sorted( zip(model_summary_dest['feature_importances'],model_summary_dest['training_columns']), reverse=True ) )
+        model_summary_dest['top5_features'] = [(feature[i],importance[i]) for i in xrange(5)]
+        model_summaries['destination'] = model_summary_dest
+
     ### Render the page
     return render_template('results.html',
+                           title='Results',
                            request_info=request_info,
-                           flightstats=flightstats)
+                           flightstats=flightstats,
+                           model_summaries=model_summaries)
     
 
 
