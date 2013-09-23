c0d8141e4e97bef2d0051741d4f3d45b8a5a4380
Mike Kuhlen <kuhlen@gmail.com>
2013-09-23 13:56:52 -0700

    Added a date validator to enforce dates today or later.

--- webpage/app/forms.py
+++ webpage/app/forms.py
@@ -1,9 +1,14 @@
 from flask.ext.wtf import Form
 from wtforms import SelectField, TextField, IntegerField
 from wtforms.ext.dateutil.fields import DateField
-from wtforms.validators import Required, Optional, NumberRange, Regexp, AnyOf
-from datetime import datetime
+from wtforms.validators import Required, Optional, NumberRange, Regexp, AnyOf, ValidationError
+from datetime import date
 import my_utils as mu
+
+def date_after_today(form, field):
+    days_from_today = (field.data - date.today()).days
+    if days_from_today < 0:
+        raise ValidationError('Date must be today or later.')
 
 class LookingForFlightForm(Form):
 
@@ -12,7 +17,7 @@ class LookingForFlightForm(Form):
     airport_values = airport_dict_name.keys() + airport_dict_name.values() + airports
     airport_validator = AnyOf(airport_values, message=u'Invalid input, must be an airport name.')
     
-    date = DateField('Date', validators = [Required()])
+    date = DateField('Date', validators = [Required(),date_after_today])
     origin = TextField('Origin', validators = [Required(),airport_validator])
     destination = TextField('Destination', validators = [Required(),airport_validator])
     
@@ -23,6 +28,6 @@ class AlreadyHaveAFlightForm(Form):
     carrier_values = carrier_dict_name.keys() + carrier_dict_name.values() + carriers
     carrier_validator = AnyOf(carrier_values, message=u'Invalid input, must be a carrier name.')
     
-    date = DateField('Date', validators = [Required()])
+    date = DateField('Date', validators = [Required(),date_after_today])
     carrier = TextField('Carrier', validators = [Required(),carrier_validator])
     flightnumber = IntegerField('FlightNumber', validators = [Required(message=u'This field is required, and must be an integer.'),NumberRange(min=1, message=u'Invalid input, must be greater than zero.')])
